1cee3b5d0e88f133f9c7d740b87aa53a2665f368
zahoff <114179073+zahoff@users.noreply.github.com>
2023-02-01 02:18:18 +0500

    Add files via upload

--- regular.py
+++ regular.py
@@ -1,32 +1,34 @@
 import re
 from pprint import pprint
+from typing import List
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
 
 with open("phonebook_raw.csv") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
+    contacts_list3 = []
 
-c_l = []
 #разбираемся с фамилия,имя,отчество, так, чтобы в итоге все были ф,и,о
 def fiochange(input):
     t1 = re.sub(r'(\w+)\s(\w+)\s(\w*)[,;][,;][,;](\w*)[,;]([\w ]*)[,;]([0-9\-\+\(\)доб\. ]*)[,;]([a-zA-Z0-9_\.]*@?[a-zA-Z0-9]*\.?[a-zA-Z]*)',
     r'\1;\2;\3;\4;\5;\6;\7;', input)
     t2 = re.sub(r'(\w+)[,;](\w+)\s(\w*)[,;][,;](\w*)[,;]([\w ]*)[,;]([0-9\-\+\(\)доб\. ]*)[,;]([a-zA-Z0-9_\.]*@?[a-zA-Z0-9]*\.?[a-zA-Z]*)',
     r'\1;\2;\3;\4;\5;\6;\7;', t1)
-    t3 = re.sub(r'(\w+)\s(\w+)[,;](\w*)[,;](\w*)[,;]([\w ]*)[,;]([0-9\-\+\(\)доб\. ]*)[,;]([a-zA-Z0-9_\.]*@?[a-zA-Z0-9]*\.?[a-zA-Z]*)',
+    t3 = re.sub(r'(\w+)\s(\w+)[,;][,;](\w*)[,;](\w*)[,;]([\w ]*)[,;]([0-9\-\+\(\)доб\. ]*)[,;]([a-zA-Z0-9_\.]*@?[a-zA-Z0-9]*\.?[a-zA-Z]*)',
     r'\1;\2;\3;\4;\5;\6;\7;', t2)
     return(t3)
 
 # TODO 1: выполните пункты 1-3 ДЗ
 for k in range(12):
     contacts_list1 = fiochange(contacts_list[k][0])
+
 # избавляемся от скобок минусов и плюсов ( у некоторых написано 7 у некоторых +7)
     contacts_list2 = re.sub(r"[\(\)\-\+]*", "", contacts_list1)
-    c_l.append(contacts_list2)
+    contacts_list3.append(contacts_list2)
 
 #борьба с пробелами в телефоне, но не в должности
-    my_string = c_l[k]
+    my_string = contacts_list3[k]
     start = -1
     count = 0
     z=[]
@@ -39,47 +41,47 @@ for k in range(12):
 
     tel = ""
     for i in range(z[4],z[5]):
-        if c_l[k][i] != " ":
-            tel += c_l[k][i]
+        if contacts_list3[k][i] != " ":
+            tel += contacts_list3[k][i]
+
 # выставляем формат телефона
     pattern = r"(7|8)(\d{3})(\d{3})(\d{2})(\d{2})(доб.\d{4})?"
     res = re.sub(pattern, r"+7(\2)\3-\4-\5\6", tel)
-    c_l[k] =c_l[k][:z[4]]+res+c_l[k][z[5]:]
-    print (c_l[k])
+    contacts_list3[k] = contacts_list3[k][:z[4]]+res+contacts_list3[k][z[5]:]
 
-#сравнение на совпадение по первым двум позициям
-for i in range(12):
-    for j in range(i,12):
-        if c_l[i][0] == c_l[j][0] and c_l[i][1] == c_l[j][1]:
-            for k in range(2,8):
-                if c_l[i][k] == "":
-                    c_l[i][k] = c_l[j][k]
-            c_l[j] = ""
+del_list = []
+# объединяем информацию по людям, которые кажутся одинаковыми
+for i in range(11):
+    poz1 = [m.start() for m in re.finditer(";", contacts_list3[i])]
+    for j in range(i+1,12):
+        poz2 = [m.start() for m in re.finditer(";", contacts_list3[j])]
+        if contacts_list3[i][:poz1[1]] == contacts_list3[j][:poz2[1]]:
+            del_list.append(j)
+            for k in range(5):
+                if contacts_list3[i][poz1[k]+1:poz1[k+1]] == "":
+                    sss = ""
+                    for t in range(poz2[k]+1,poz2[k+1]):
+                        sss += contacts_list3[j][t]
+                    contacts_list3[i] = contacts_list3[i][:poz1[1]]+contacts_list3[i][poz1[k]:] + sss + contacts_list3[i][poz1[k+1]:]
 
-#print (c_l)
+for i in del_list:
+    contacts_list3[i] =""
+
+for i in range(12-len(del_list)):
+    if contacts_list3[i] == "":
+        for j in range(i,11):
+            contacts_list3[j] = contacts_list3[j+1]
+
+print (contacts_list3)
 
 
 
-
-
-
-for i in range(len(c_l[1])):
-    #pprint(c_l[1][i])
-    z = [m.start() for m in re.finditer(c_l[1],';')]
-#print(z)
-
-
-for x in contacts_list:
-    # pprint(x[0])
-    result = re.sub(r'[\(\)]', "", x[0])
-    # pprint(result)
 
     # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 with open("phonebook.csv", "w") as f:
     datawriter = csv.writer(f, delimiter=',')
     # Вместо contacts_list подставьте свой список
-    datawriter.writerows(contacts_list)
+    datawriter.writerows(contacts_list3)
 
 
-f
